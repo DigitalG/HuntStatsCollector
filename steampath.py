bacd49363f3ab27978e0de518ba12b8fdfeb2d1d
andriiromaniuk <romaniukandrii7@gmail.com>
2022-08-10 16:37:20 +0300

    added findGame function

--- steampath.py
+++ steampath.py
@@ -1,3 +1,7 @@
+import string
+import vdf
+
+
 def get_steam_path():
     import sys
     import winreg
@@ -5,3 +9,18 @@ def get_steam_path():
     hkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\WOW6432Node\Valve\Steam")
     path = winreg.QueryValueEx(hkey, "InstallPath")
     return path[0]
+
+
+def findGame(gameIdToFind: string):
+    vdfPath = get_steam_path() + "\steamapps\libraryfolders.vdf"
+    vdfFile = vdf.load(open(vdfPath))
+    steamLibs = vdfFile["libraryfolders"]
+    for i in steamLibs:
+        steamApps = steamLibs[i]["apps"]
+        for gameId in steamApps:
+            if gameId == gameIdToFind:
+                return steamLibs[i]["path"]
+
+
+if __name__ == "__main__":
+    print(findGame('594650'))
